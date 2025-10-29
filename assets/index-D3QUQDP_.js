3f174c3f8ac943fe20ff46702abe209b4f8655c8
uso538 <uso538@gmail.com>
2025-10-29 17:03:09 +0900

    Updates

--- assets/index-D3QUQDP_.js
+++ assets/index-D3QUQDP_.js
@@ -226,60 +226,67 @@ X: 1
 M: 4/4
 L: 1/8
 K: C
-|: "CM7" CEGB "Dm7" DFAc | "Em7" EGBd "FM7" FAce | "G7" GBdf "Am7" Aceg | "Bm7b5" Bdfa "CM7" cegb |
+"CM7" CEGB "Dm7" DFAc | "Em7" EGBd "FM7" FAce | "G7" GBdf "Am7" Aceg | "Bm7b5" Bdfa "CM7" cegb |
 w:       ⑥
-"CM7" c'bge "Bm7b5" bafd | "Am7" agec "G7" gfdB | "FM7" fecA "Em7" edBG | "Dm7" dcAF "CM7" cBGE :|
+"CM7" c'bge "Bm7b5" bafd | "Am7" agec "G7" gfdB | "FM7" fecA "Em7" edBG | "Dm7" dcAF "CM7" cBGE |
 
 `},{title:"練習2",abcNotation:`
 X: 1
 M: 4/4
 L: 1/8
 K: C
-|: "Dm7" A,CDF Acdf | "G7" gbgf dBGF | "CM7" ECB,G, B,CEG | Bceg bc'bg |
+"CM7" [CE]GBd "Dm7" [DF]Ace | "Em7" [EG]Bdf "FM7" [FA]ceg | "G7" [GB]dfa "Am7" [Ac]egb | "Bm7b5" [Bd]fac' "CM7" [ce]gbd' |
+
+`},{title:"練習3",abcNotation:`
+X: 1
+M: 4/4
+L: 1/8
+K: C
+"Dm7" A,CDF Acdf | "G7" gbgf dBGF | "CM7" ECB,G, B,CEG | Bceg bc'bg |
 w:       ⑥
-"Dm7" fdcA FDCA, | "G7" G,B,DF GBdf | "CM7" gbc'b gecB | GECB, G,B,CE :|
+"Dm7" fdcA FDCA, | "G7" G,B,DF GBdf | "CM7" gbc'b gecB | GECB, G,B,CE |
 
 `},{title:"II-V 1",abcNotation:`
 X: 1
 M: 4/4
 L: 1/8
 K: C
-|: "Dm7" dfac' "G7" b_ba_a | "CM7" g8 :|
+"Dm7" dfac' "G7" b_ba_a | "CM7" g8 |
     
 `},{title:"II-V 2",abcNotation:`
 X: 1
 M: 4/4
 L: 1/8
 K: C
-|: "Dm7" afdc "G7" B_B_A^F | "CM7" G8 :|
+"Dm7" afdc "G7" B_B_A^F | "CM7" G8 |
     
 `},{title:"II-V 3",abcNotation:`
 X: 1
 M: 4/4
 L: 1/8
 K: C
-|: "Dm7" DEFG AFED | "G7" _A_B_c_d _e_c_B_A | "CM7" G8 :|
+"Dm7" DEFG AFED | "G7" _A_B_c_d _e_c_B_A | "CM7" G8 |
     
 `},{title:"II-V 4",abcNotation:`
 X: 1
 M: 4/4
 L: 1/8
 K: C
-|: "Dm7" DEFG ABcd | "G7" ed^cB _B_AGF | "CM7" E8 :|
+"Dm7" DEFG ABcd | "G7" ed^cB _B_AGF | "CM7" E8 |
     
 `},{title:"II-V 5",abcNotation:`
 X: 1
 M: 4/4
 L: 1/8
 K: C
-|: "Dm7" DFGA cdfg | "G7" agfd _d_AGF | "CM7" E8 :|
+"Dm7" DFGA cdfg | "G7" agfd _d_AGF | "CM7" E8 |
     
 `},{title:"II-V 6",abcNotation:`
 X: 1
 M: 4/4
 L: 1/8
 K: C
-|: "Dm7" bc'af edge | "G7" fcAF _e2 BG | "CM7" dBGE A4 :|
+"Dm7" bc'af edge | "G7" fcAF _e2 BG | "CM7" dBGE A4 |
     
 `},{title:"Sample Tune",abcNotation:`
 X:1
